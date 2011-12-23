38b180951dc7a25cfd7382d19eb8c0055a3fa6aa
Zealot Ke <chzealot@gmail.com>
2011-12-23 10:06:47 +0800

    add installer
    Signed-off-by: Zealot Ke <chzealot@gmail.com>

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#===============================================================================
+#          FILE:  install.sh
+# 
+#         USAGE:  ./install.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Zealot Ke (Zealot), chzealot@gmail.com
+#       COMPANY:  losthit.com
+#       VERSION:  1.0
+#       CREATED:  12/23/2011 09:44:54 CST
+#      REVISION:  ---
+#===============================================================================
+
+
+TARGET_PATH="/tmp/install_bash_tookit"
+TARGET_FILE="bash-toolkit.tar.gz"
+TARGET_DIR="bash-toolkit"
+TARGET_LINK="https://github.com/chzealot/bash-toolkit/tarball/master"
+WGET="`which wget|awk -F" " '{print $1}'` --no-check-certificate"
+MKDIR="`which mkdir|awk -F" " '{print $1}'`"
+MV="`which mv|awk -F" " '{print $1}'`"
+RM="`which rm|awk -F" " '{print $1}'`"
+TAR="`which tar|awk -F" " '{print $1}'`"
+MAKE="`which make|awk -F" " '{print $1}'`"
+CWD=`pwd`
+
+# download
+if [[ ! -d $TARGET_PATH ]]; then
+    $MKDIR -p $TARGET_PATH
+fi
+cd $TARGET_PATH
+$WGET "$TARGET_LINK" -O $TARGET_FILE
+
+# extract
+$RM -rf $TARGET_DIR
+$RM -rf chzealot-bash-toolkit-*
+$TAR zxvf $TARGET_FILE
+$MV chzealot-bash-toolkit-* $TARGET_DIR
+cd $TARGET_DIR
+$MAKE install
+
+cd $CWD
+
+$RM -rf $TARGET_PATH

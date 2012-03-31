a40ab63425720f0b9e3925775292fcd88710dc15
Zealot Ke <chzealot@gmail.com>
2012-03-31 11:07:17 +0800

    useful script to init git repo
    Signed-off-by: Zealot Ke <chzealot@gmail.com>

---
+++ .local/bin/init-git.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+#===============================================================================
+#          FILE:  init-git.sh
+# 
+#         USAGE:  ./init-git.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Zealot Ke (Zealot), chzealot@gmail.com
+#       COMPANY:  losthit.com
+#       VERSION:  1.0
+#       CREATED:  03/31/2012 10:44:03 CST
+#      REVISION:  ---
+#===============================================================================
+
+BASE_PATH=$(cd "$(dirname "$0")"; pwd)
+
+PROJECT_NAME=$(basename `pwd`)
+
+git init-db
+
+git config --local --replace-all core.bare true
+
+git config --local --add hooks.emailprefix "[$PROJECT_NAME]"
+
+git config --local --add hooks.mailinglist "chzealot@gmail.com"
+
+echo "$PROJECT_NAME" > .git/description
+
+echo ". /usr/share/doc/git-core/contrib/hooks/post-receive-email" >>.git/hooks/post-receive
+
+chmod +x .git/hooks/post-receive
+
+mv .git $PROJECT_NAME.git
+
+tar jcvf $PROJECT_NAME.git.tar.bz2 $PROJECT_NAME.git
